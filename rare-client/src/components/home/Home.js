5cb1743f900d3ff787f7e37df1acbc14ec21e10e
kaylamckenzie <kmckenzie82@bethelu.edu>
2026-05-05 13:56:14 -0500

    feat(home): improve empty state for subscription feed

--- rare-client/src/components/home/Home.js
+++ rare-client/src/components/home/Home.js
@@ -13,7 +13,20 @@ export const Home = () => {
     <div className="container">
       <h2 className="title is-4 mt-4">Posts from Subscriptions</h2>
       {subscribedPosts.length === 0 ? (
-        <p>No posts from subscribed authors yet.</p>
+        <div className="box mt-4">
+          <p className="title is-5">Your subscription feed is empty</p>
+          <p>
+            This page shows the latest posts from authors you subscribe to. Once you
+            follow someone, their new posts will appear here.
+          </p>
+          <p className="mt-3">
+            Browse the <Link to="/posts">Posts page</Link> to discover authors and
+            content you enjoy. You can subscribe to an author from any of their posts.
+          </p>
+          <Link to="/posts" className="button is-primary mt-4">
+            Browse Posts
+          </Link>
+        </div>
       ) : (
         <table className="table is-fullwidth is-striped">
           <thead>
